645317e02a49484177c627670d0705683e50008f
songyang <songyang8761@qq.com>
2016-09-09 22:20:27 +0800

    Initial commit

---
+++ src/com/songyang/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.songyang.demo;
+
+/**
+ * Created by songyang on 2016/9/9.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
